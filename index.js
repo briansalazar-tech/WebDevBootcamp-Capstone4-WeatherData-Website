f30e522475b2488d660d115014a2473b97fcee96
Brian Salazar <briansalazar5@gmail.com>
2026-03-26 15:43:54 -0700

    Add code necessary to render webpage

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
+import express from "express";
+import axios from "axios";
 import dotenv from "dotenv";
+
 dotenv.config();
 
+const API_URL = "";
 const API_KEY = process.env.API_KEY;
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.get("/",  (req, res) => {
+    res.render("index.ejs");
+})
+
+app.listen(port, () => {
+    console.log(`listening on port: ${port}`)
+})
