0261b3b6bcf61be12be0cce346fcaf836e444580
Brian Salazar <briansalazar5@gmail.com>
2026-03-26 16:42:00 -0700

    Capture temp, timestamps and descriptions to pass to website

--- index.js
+++ index.js
@@ -4,15 +4,52 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const API_URL = "";
+const LON = process.env.LON;
+const LAT = process.env.LAT;
 const API_KEY = process.env.API_KEY;
+const API_URL = "https://api.openweathermap.org/data/2.5/forecast";
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
 
-app.get("/",  (req, res) => {
-    res.render("index.ejs");
+app.get("/", async (req, res) => {
+    const dateTimes = [];
+    const temps = [];
+    const descriptions = [];
+    
+
+    try {
+        const response = await axios.get(API_URL, {
+            params: {
+                lat: LAT,
+                lon: LON,
+                appid: API_KEY,
+                units: "imperial"
+            }
+        });
+        // Loop through each weather data entry
+        response.data.list.forEach((entry) => {
+            dateTimes.push(entry.dt_txt);
+            temps.push(entry.main.temp.toString());
+            descriptions.push(entry.weather[0].description);
+        });
+        // console.log(dateTimes);
+        // console.log(temps);
+        // console.log(descriptions);
+
+        // data to be passed to web page
+        const content = {
+            dateTimes: dateTimes,
+            temps: temps,
+            descriptions: descriptions
+        };
+        // console.log(content)
+        res.render("index.ejs");
+    } catch (error) {
+        console.log(error.message);
+    }
+    
 })
 
 app.listen(port, () => {
